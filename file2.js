7b873088a2be5e5d1cced1762ea3a2752fc6575e
zakir <zakirmustafa444@gmaol.com>
2023-11-30 11:27:16 +0530

    my ist commit

---
+++ file2.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+// fs.mkdir("myfolder", function(er){
+//     console.log("flder created");
+// });
+// fs.writeFile("./myfolder/myfile.txt","my name is zakir",(er)=>{
+//     console.log("file created");
+// })
+// fs.appendFile("./myfolder/myfile.txt","and i live at akhran",function(er){
+//     console.log("more data added");
+// })
+// fs.readFile("./myfolder/myfile.txt",(er,data) => {
+//     console.log(data.toString());
+// })
+// fs.rename("./myfolder/myfile.txt","./myfolder/urfile.txt",function(er){
+//     console.log("name changed");
+// })
+// fs.rmdir("myfolder",(er) => {
+//     console.log("folder deleated");
+// })
